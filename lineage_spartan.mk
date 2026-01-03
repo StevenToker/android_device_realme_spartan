aea1d97dd032749bc38eff8460a5528d043de9bd
Steve <146429562+StevenToker@users.noreply.github.com>
2026-01-03 18:16:12 +0530

    Matrixx Vanilla

--- lineage_spartan.mk
+++ lineage_spartan.mk
@@ -21,6 +21,9 @@ PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3371
 
+# To include Gapps 
+WITH_GMS := false
+
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
