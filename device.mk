ea3d0ab6513265d120960887b0eb760ce086ed9e
Steve <146429562+StevenToker@users.noreply.github.com>
2026-01-03 18:56:31 +0530

    Speed profile services and wifi-service to reduce RAM and storage

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# ART
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
